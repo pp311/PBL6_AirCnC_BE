2ab1c9785d1471a177abe30bfafad9c44feb165b
pp311 <phucnhphc@gmail.com>
2023-10-08 01:53:30 +0700

    feat(property): advance filter for property list

--- AirCnc_Backend/AirCnC.Application/Services/PropertyService/Specifications/PropertyFilterSpecification.cs
+++ AirCnc_Backend/AirCnC.Application/Services/PropertyService/Specifications/PropertyFilterSpecification.cs
@@ -1,42 +1,92 @@
-using AirCnC.Application.Services.PropertyService.Enums;
+using AirCnC.Application.Services.PropertyService.Dtos;
 using AirCnC.Domain.Entities;
-using AirCnC.Domain.Enums;
 using AirCnC.Domain.Specification;
 
 namespace AirCnC.Application.Services.PropertyService.Specifications;
 
 public class PropertyFilterSpecification : Specification<Property>
 {
-    public PropertyFilterSpecification(string? search, 
-                                       PropertyType? type,
-                                       int pageSize,
-                                       int pageIndex,
-                                       PropertySortBy? orderBy, 
-                                       bool isDescending)
+    public PropertyFilterSpecification(PropertyQueryParameters pqp)
     {
         AddInclude(p => p.PropertyImages);
-        // 1. Search by name, description
-        if (!string.IsNullOrWhiteSpace(search))
+        // Search by name, description, city
+        if (!string.IsNullOrWhiteSpace(pqp.Search))
         {
-            AddSearchTerm(search);
+            AddSearchTerm(pqp.Search);
             AddSearchField(nameof(Property.Title));
             AddSearchField(nameof(Property.Description));
+            AddSearchField(nameof(Property.City));
         }
         
-        // 2. Filter by type
-        if (type != null)
-            AddFilter(p => p.Type == type);
+        if (pqp.Type != null && pqp.Type.Any())
+            AddFilter(p => pqp.Type.Contains(p.Type));
+        
+        FilterByPriceRange(pqp);
 
-        // 3. Order by
-        if (orderBy is not null)
+        FilterByRoomAndBedCount(pqp);
+
+        if (!string.IsNullOrWhiteSpace(pqp.City))
+            AddFilter(p => p.City == pqp.City);
+        
+        FilterByDateRange(pqp);
+
+        FilterByGuestCount(pqp);
+
+        // Order by
+        if (pqp.OrderBy is not null)
         {
-            AddOrderByField(orderBy.ToString());
-            if (isDescending)
+            AddOrderByField(pqp.OrderBy.ToString());
+            if (pqp.IsDescending)
                 ApplyDescending();
         }
         
-        
-        // 4. Paging
-        ApplyPaging(pageIndex: pageIndex, pageSize: pageSize);
-    } 
+        ApplyPaging(pageIndex: pqp.PageIndex, pageSize: pqp.PageSize);
+    }
+
+    private void FilterByGuestCount(PropertyQueryParameters pqp)
+    {
+        // Filter by adult count
+        if (pqp.AdultCount > 0)
+            AddFilter(p => p.MaxAdultCount >= pqp.AdultCount);
+
+        // Filter by child count
+        if (pqp.ChildCount > 0)
+            AddFilter(p => p.MaxChildCount >= pqp.ChildCount);
+    }
+
+    private void FilterByDateRange(PropertyQueryParameters pqp)
+    {
+        if (pqp is { CheckInDate: not null, CheckOutDate: not null })
+        {
+            AddFilter(p => p.Bookings.All(r => r.CheckInDate > pqp.CheckOutDate || r.CheckOutDate < pqp.CheckInDate));        
+        }
+    }
+
+    private void FilterByRoomAndBedCount(PropertyQueryParameters pqp)
+    {
+        if (pqp.MinBedroomCount > 0)
+            AddFilter(p => p.BedroomCount >= pqp.MinBedroomCount);
+        if (pqp.MaxBedroomCount > 0)
+            AddFilter(p => p.BedroomCount <= pqp.MaxBedroomCount);
+
+        // Filter by bed count range
+        if (pqp.MinBedCount > 0)
+            AddFilter(p => p.BedCount >= pqp.MinBedCount);
+        if (pqp.MaxBedCount > 0)
+            AddFilter(p => p.BedCount <= pqp.MaxBedCount);
+
+        // Filter by bathroom count range
+        if (pqp.MinBathroomCount > 0)
+            AddFilter(p => p.BathroomCount >= pqp.MinBathroomCount);
+        if (pqp.MaxBathroomCount > 0)
+            AddFilter(p => p.BathroomCount <= pqp.MaxBathroomCount);
+    }
+
+    private void FilterByPriceRange(PropertyQueryParameters pqp)
+    {
+        if (pqp.MinPrice > 0)
+            AddFilter(p => p.PricePerNight >= pqp.MinPrice);
+        if (pqp.MaxPrice > 0)
+            AddFilter(p => p.PricePerNight <= pqp.MaxPrice);
+    }
 }
